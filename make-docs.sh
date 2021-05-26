9b949080d91edaa4c10760a657ebd0e9978e62c4
James Elford <james.p.elford@gmail.com>
2021-05-26 20:50:19 +0100

    feat: add some getting-started documentation that walks through a simple form-submission workflow

--- make-docs.sh
+++ make-docs.sh
@@ -5,6 +5,8 @@ set LFS=$'\t\n'
 PROJECTROOT=$(git rev-parse --show-toplevel)
 DOCSROOT="${PROJECTROOT}/docs"
 
+poetry export --dev --format requirements.txt > docs/requirements.txt
+
 cd $DOCSROOT
 rm -rf "${DOCSROOT}/source"
 sphinx-apidoc -o "${DOCSROOT}/source" --module-first "${PROJECTROOT}/src/activesoup" 
