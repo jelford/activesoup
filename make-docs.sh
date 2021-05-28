ff70745cf33158879a6eb994795ffa43090301c2
James Elford <james.p.elford@gmail.com>
2021-05-28 11:38:32 +0100

    sdlc: clean docs directory as part of building

--- make-docs.sh
+++ make-docs.sh
@@ -10,6 +10,8 @@ poetry export --dev --format requirements.txt > docs/requirements.txt
 cd $DOCSROOT
 rm -rf "${DOCSROOT}/source"
 
+make clean
+
 export SPHINX_APIDOC_OPTIONS="members,no-undoc-members,show-inheritance"
 sphinx-apidoc -o "${DOCSROOT}/source" --module-first "${PROJECTROOT}/src/activesoup" \
     "${PROJECTROOT}/src/activesoup/driver.py"  # public objects are re-exported from __init__.py
